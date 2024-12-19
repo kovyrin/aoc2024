a1369bfa36c5775c1b1856edda2292cd03c02a33
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2024-12-19 09:32:45 -0500

    Day 19 part 2

--- day19/d19p2.rb
+++ day19/d19p2.rb
@@ -1,4 +1,42 @@
 #! /usr/bin/env ruby
 
+def count_arrangements(suffix: design, towels:, count_cache: {})
+  return 1 if suffix.empty?
+  return count_cache[suffix] if count_cache.key?(suffix)
+
+  # Check if any of the towels match the beginning of the design
+  matching_towels = towels.select { |towel| suffix.start_with?(towel) }
+  return 0 if matching_towels.empty?
+
+  result = matching_towels.sum do |towel|
+    remaining_design = suffix[towel.size..]
+    count_arrangements(suffix: remaining_design, towels:, count_cache:)
+  end
+
+  count_cache[suffix] = result
+  result
+end
+
 input_file = ENV['REAL'] ? "input.txt" : "input-demo.txt"
 lines = File.readlines(input_file)
+
+towels = lines.first.split(',').map(&:strip)
+designs = lines[2..].map(&:strip)
+
+arrangements_count = 0
+designs.each do |design|
+  puts "Checking design: #{design}"
+  count = count_arrangements(suffix: design, towels:)
+
+  if count > 0
+    puts "  - Found #{count} possible arrangements"
+    arrangements_count += count
+  else
+    puts "  - No possible arrangements found"
+  end
+end
+
+puts
+puts "Total arrangements: #{arrangements_count}"
+
+# 848076019766013 - correct
