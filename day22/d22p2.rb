91538105700d32b42ebefb67cb93add35decdcbf
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2024-12-22 10:27:59 -0500

    Day 22 done

--- day22/d22p2.rb
+++ day22/d22p2.rb
@@ -1,4 +1,69 @@
 #! /usr/bin/env ruby
 
-input_file = ENV['REAL'] ? "input.txt" : "input-demo.txt"
-lines = File.readlines(input_file)
+class RandomGenerator
+  def initialize(seed)
+    @seed = seed
+  end
+
+  def next
+    a = @seed * 64
+    mix(a)
+    prune!
+
+    b = @seed / 32
+    mix(b)
+    prune!
+
+    c = @seed * 2048
+    mix(c)
+    prune!
+
+    @seed
+  end
+
+  def mix(a)
+    @seed = @seed ^ a
+  end
+
+  def prune!
+    @seed = @seed % 16_777_216
+  end
+end
+
+#---------------------------------------------------------------------------------
+input_file = ENV['REAL'] ? "input.txt" : "input-demo2.txt"
+seeds = File.readlines(input_file).map(&:strip).reject(&:empty?).map(&:to_i)
+
+shared_ngrams = Hash.new(0)
+
+seeds.each do |seed|
+  random_generator = RandomGenerator.new(seed)
+  previous_price = seed % 10
+  price_diff_pairs = []
+
+  2000.times do
+    number = random_generator.next
+    price = number % 10
+    diff = price - previous_price
+    previous_price = price
+    price_diff_pairs << {price: price, diff: diff}
+  end
+
+  # generate all price n-grams of length 4 and record each n-gram with the price at the end of the n-gram
+  ngram_length = 4
+  seen_ngrams = Set.new
+  (0..price_diff_pairs.size - ngram_length).each do |start_index|
+    window = price_diff_pairs[start_index, ngram_length]
+    price = window.last[:price]
+    ngram = window.map { |p| p[:diff] }
+
+    next if seen_ngrams.include?(ngram)
+    seen_ngrams.add(ngram)
+
+    shared_ngrams[ngram] += price
+  end
+end
+
+best_price = shared_ngrams.values.max
+best_ngram = shared_ngrams.key(best_price)
+puts "Best ngram: #{best_ngram.join(',')} -> #{best_price}"
